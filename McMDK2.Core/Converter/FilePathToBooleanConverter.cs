876a03dc0f985e62a80fc25b715e0f47adba21c6
tuyapin <tuyapin0130@gmail.com>
2014-11-04 19:32:32 +0900

    Added annotation.

---
+++ McMDK2.Core/Converter/FilePathToBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace McMDK2.Core.Converter
+{
+    public class FilePathToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo cultureInfo)
+        {
+            var path = (string)value;
+            if (FileController.Exists(path))
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo cultureInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
