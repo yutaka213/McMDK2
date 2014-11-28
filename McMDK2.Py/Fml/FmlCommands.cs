8cfa9a2ad1fbdab02d175b58ea5a889893b1164e
tuyapin <tuyapin0130@gmail.com>
2014-11-28 22:47:40 +0900

    Add McMDK2.Py Project.

---
+++ McMDK2.Py/Fml/FmlCommands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McMDK2.Py.Fml
+{
+    /// <summary>
+    /// Imitate the forge/fml/fml.py's workings.
+    /// </summary>
+
+    class FmlCommands
+    {
+    }
+}
